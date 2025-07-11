e7fac5d74e024fc4a40e290788cf41ed36ce2866
claudismar <claudismar.aleixo@gmail.com>
2025-07-11 17:19:12 -0300

    test: Criação das classes de teste unitário e teste de integração.

---
+++ src/test/java/br/com/cotiinformatica/controllers/PedidosControllerTest.java
@@ -0,0 +1,50 @@
+package br.com.cotiinformatica.controllers;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PedidosControllerTest {
+
+	@BeforeEach
+	public void setUp() {
+		// TODO preparar o ambiente de testes
+	}
+	
+	@Test
+	@DisplayName("Deve executar POST /api/v1/pedidos com sucesso (201).")
+	public void testPostPedido_Sucesso() {
+		// TODO implementar o teste para criar um pedido com sucesso
+		fail("Não implementado ainda.");
+	}
+	
+	@Test
+	@DisplayName("Deve executar PUT /api/v1/pedidos com sucesso (200).")
+	public void testPutPedido_Sucesso() {
+		// TODO implementar o teste para atualizar um pedido com sucesso
+		fail("Não implementado ainda.");
+	}
+	
+	@Test
+	@DisplayName("Deve executar DELETE /api/v1/pedidos com sucesso (200).")
+	public void testDeletePedido_Sucesso() {
+		// TODO implementar o teste para excluir um pedido com sucesso
+		fail("Não implementado ainda.");
+	}
+	
+	@Test
+	@DisplayName("Deve executar GET ALL /api/v1/pedidos/{id} com sucesso (200).")
+	public void testGetAllPedido_Sucesso() {
+		// TODO implementar o teste para buscar um pedido por ID com sucesso
+		fail("Não implementado ainda.");
+	}
+	
+	@Test
+	@DisplayName("Deve executar GET BY ID /api/v1/pedidos com sucesso (200).")
+	public void testGetByIdPedido_Sucesso() {
+		// TODO implementar o teste para buscar todos os pedidos com sucesso
+		fail("Não implementado ainda.");
+	}
+}
